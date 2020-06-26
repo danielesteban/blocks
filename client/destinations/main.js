9fe4937b32c5adb81ae6014479fd8b15108e0efe
Daniel Esteban Nombela <dani@gatunes.com>
2020-06-26 13:40:02 +0200

    second draft of the destinations UI. Part of #21

--- client/destinations/main.js
+++ client/destinations/main.js
@@ -1,18 +1,22 @@
 const authService = 'https://blocks.gatunes.com/auth/';
 
-const [image] = document.getElementById('photo').getElementsByTagName('img');
+const photo = document.getElementById('photo');
+const [link] = photo.getElementsByTagName('a');
+const [image] = photo.getElementsByTagName('img');
 const meta = document.getElementById('meta');
-const [title, user] = meta.getElementsByTagName('div');
+const [title, sharing, user] = meta.getElementsByTagName('div');
+const [twitter, facebook] = sharing.getElementsByTagName('a');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 
-let locations;
 let current;
+const locations = [];
 
 const renderLocation = (index) => {
   current = index;
   const location = locations[index];
   image.src = `${authService}location/${location._id}/photo`;
+  image.style.display = '';
   const leadingZero = (v) => (v.length < 2 ? `0${v}` : v);
   const createdAt = new Date(location.createdAt);
   title.innerText = (
@@ -24,19 +28,19 @@ const renderLocation = (index) => {
     + ` - ${createdAt.getFullYear()}/${leadingZero(`${createdAt.getMonth() + 1}`)}/${leadingZero(`${createdAt.getDate()}`)}`
     + ` ${leadingZero(`${createdAt.getHours()}`)}:${leadingZero(`${createdAt.getMinutes()}`)}`
   );
+  link.href = `/#/location:${location._id}`;
+  const permalink = encodeURIComponent(`${authService}location/${location._id}`);
+  facebook.href = `https://facebook.com/sharer/sharer.php?u=${permalink}`;
+  twitter.href = `https://twitter.com/intent/tweet?url=${permalink}`;
 };
 
-const fetchLocations = (page = 0) => (
-  fetch(`${authService}locations?page=${page}`)
-    .then((res) => res.json())
-    .then((json) => {
-      locations = json;
-      renderLocation(0);
-    })
-);
-
-fetchLocations();
-
+[facebook, twitter].forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.open(link.href, '', 'width=640,height=720');
+  });
+  link.target = '_blank';
+});
 next.className = 'enabled';
 next.addEventListener('click', () => {
   if (next.className !== 'enabled') {
@@ -45,7 +49,6 @@ next.addEventListener('click', () => {
   prev.className = 'enabled';
   renderLocation(current + 1);
   if (current === locations.length - 1) {
-    // TODO: fetch next page!
     next.className = '';
   }
 });
@@ -59,3 +62,27 @@ prev.addEventListener('click', () => {
     prev.className = '';
   }
 });
+
+const fetchLocations = (page = 1) => (
+  fetch(`${authService}locations?page=${page}`)
+    .then((res) => (
+      res
+        .json()
+        .then((entries) => ({
+          entries,
+          pages: parseInt(res.headers.get('X-Total-Pages'), 10),
+        }))
+    ))
+    .then(({ entries, pages }) => {
+      locations.push(...entries);
+      if (page === 1) {
+        renderLocation(0);
+      }
+      if (page < Math.min(pages, 5)) {
+        return fetchLocations(page + 1);
+      }
+      return false;
+    })
+);
+
+fetchLocations();
