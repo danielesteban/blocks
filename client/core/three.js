7249b882b442c024eadbc153e094029a731c1a2f
Daniel Esteban Nombela <dani@gatunes.com>
2020-06-28 12:43:46 +0200

    Update dependencies

--- client/core/three.js
+++ client/core/three.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/no-unresolved */
-export * from 'https://cdn.jsdelivr.net/npm/three@0.118.1/build/three.module.js';
-export { BufferGeometryUtils } from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/utils/BufferGeometryUtils.js';
-export { SimplexNoise } from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/math/SimplexNoise.js';
+export * from 'https://cdn.jsdelivr.net/npm/three@0.118.3/build/three.module.js';
+export { BufferGeometryUtils } from 'https://cdn.jsdelivr.net/npm/three@0.118.3/examples/jsm/utils/BufferGeometryUtils.js';
+export { SimplexNoise } from 'https://cdn.jsdelivr.net/npm/three@0.118.3/examples/jsm/math/SimplexNoise.js';
